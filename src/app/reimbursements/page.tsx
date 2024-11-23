adf08457591057c5ecb5a7d33929b5226e6b0756
dkupper4 <dkupper4@gmail.com>
2024-11-23 15:21:19 -0500

    Table for reimbursements

--- src/app/reimbursements/page.tsx
+++ src/app/reimbursements/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import React from "react";
 import "./reimbursements.css";
+import { DataTable } from "../components/DataTable";
+
 
 function Reimbursements() {
     return (
         <div className="text">
-            Reimbursements
+            <h1>Reimbursements</h1>  
+                <br /> 
+                <div className="content-container">       
+                    <DataTable />
+                </div>
         </div>
     );
 }
