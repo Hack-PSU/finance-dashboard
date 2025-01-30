daca884bee8840429642e05142ee0197daf767f4
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-01-29 22:33:17 -0500

    fix redirect bug?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import React from "react";
 import SideNavbar from "../components/DashboardLayout/DashboardLayout";
-import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/reimbursements");
   return (
     <div className="App">
       <SideNavbar />
